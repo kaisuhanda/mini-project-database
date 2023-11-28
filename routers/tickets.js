b89cbcf3364e189677d387375828b69f76c8d5c3
khalid1200 <khalidabdula28@gmail.com>
2023-11-28 12:47:14 +0700

    update fix

--- routers/tickets.js
+++ routers/tickets.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const { ticketsController } = require("../controllers");
-
+const { validateToken } = require("../middleware/validation")
 const router = express.Router();
-
+// kai
 router.get('/', ticketsController.getData);
 
 // khalid
 router.post('/:id', ticketsController.createTicket);
 router.patch('/:id', ticketsController.updateTicket);
 router.delete('/:id', ticketsController.deleteTicket);
+
+// nanda
+router.post('/transactions', validateToken, ticketsController.createTransaction);
+router.get('/transactions/:userId', ticketsController.getTransactionsByUserId);
 
 module.exports = router;
